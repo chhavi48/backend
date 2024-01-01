900f5905d8a7073ef0b0419842ec949e1ff5660f
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2024-01-01 14:33:22 +0530

    FinalPush

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
+const app = express();
 require("dotenv").config();
 
-const app = express();
-const PORT = 8080;
+const cryptocurrenciesRouter = require("./routes/cryptocurrencies");
+const convertRouter = require("./routes/convert");
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -21,58 +23,9 @@ const addApiKey = (req, res, next) => {
 // Apply the middleware to all routes
 app.use(addApiKey);
 
-app.get("/api/cryptocurrencies", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets",
-      {
-        params: {
-          vs_currency: "usd",
-          order: "market_cap_desc",
-          per_page: 10,
-          page: 1,
-          sparkline: false,
-        },
-      }
-    );
-
-    const cryptocurrencies = response.data.map((crypto) => ({
-      id: crypto.id,
-      name: crypto.name,
-      symbol: crypto.symbol,
-    }));
-
-    res.json(cryptocurrencies);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-app.post("/api/convert", async (req, res) => {
-  try {
-    const { currencyId, amount, targetCurrency } = req.body;
-
-    const response = await axios.get(
-      "https://api.coingecko.com/api/v3/simple/price",
-      {
-        params: {
-          ids: currencyId,
-          vs_currencies: targetCurrency,
-        },
-      }
-    );
-
-    const exchangeRate = response.data[currencyId][targetCurrency];
-
-    const convertedAmount = amount * exchangeRate;
-
-    res.json({ convertedAmount });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// Routes
+app.use("/api/cryptocurrencies", cryptocurrenciesRouter);
+app.use("/api/convert", convertRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
