e7903f72eab46be89a62679c9a3126568b9bd726
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2024-01-26 11:22:43 +0530

    loginwithSignup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const ConnectDb = require("./config/dbConnection");
 require("dotenv").config();
+const emailSender = require("./routes/emailSender");
 const app = express();
 
 app.use(cors());
@@ -28,7 +29,7 @@ app.use(addApiKey);
 app.use("/api/cryptocurrencies", require("./routes/cryptocurrencies"));
 app.use("/api/convert", require("./routes/convert"));
 app.use("/api/users", require("./routes/user"));
-
+emailSender();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ routes/emailSender.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter using your email service provider's SMTP settings
+
+const emailSender=()=>{
+
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'sichhavi48@gmail.com', // Your email address
+          pass: 'hzeq fxsh kdgu iuht' // Your email password (use an app-specific password for security)
+        }
+      });
+      
+      // Email options
+      const mailOptions = {
+        from: 'sichhavi48@gmail.com', // Sender's email address
+        to: 'exam.portal.asy@gmail.com', // Recipient's email address
+        subject: 'reply from bubu', // Email subject
+        text: 'mai kyo khau tum khalo' // Email body
+      };
+      
+      // Send the email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error:', error.message);
+        } else {
+          console.log('Email sent:', info.response);
+        }
+        
+        // Close the transporter
+        transporter.close();
+      });
+      
+}
+
+module.exports =emailSender
